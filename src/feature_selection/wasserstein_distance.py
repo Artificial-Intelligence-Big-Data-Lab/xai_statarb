c1fc9d77141b1541ed5e011778e18bf8029f79a8
mariamadalina <madalinamazilu@yahoo.com>
2021-03-26 12:47:10 +0200

    refactor: function for WD

--- src/feature_selection/wasserstein_distance.py
+++ src/feature_selection/wasserstein_distance.py
@@ -5,8 +5,16 @@ from scipy.interpolate import interp1d
 
 def wasserstein_distance_from_samples(sample_p: np.ndarray,
                                       sample_q: np.ndarray) -> float:
-    return continuous_wasserstein_distance_from_sample(sample_p=sample_p,
-                                                       sample_q=sample_q)
+    kde_p, kde_q = samples_to_kde(sample_p, sample_q)
+    return wasserstein_distance_from_kde(kde_p, kde_q)
+
+
+def samples_to_kde(sample_p, sample_q) -> (sm.nonparametric.KDEUnivariate, sm.nonparametric.KDEUnivariate):
+    kde_p = sm.nonparametric.KDEUnivariate(sample_p)
+    kde_p.fit()
+    kde_q = sm.nonparametric.KDEUnivariate(sample_q)
+    kde_q.fit()
+    return kde_p, kde_q
 
 
 def wasserstein_distance_from_kde(p: sm.nonparametric.KDEUnivariate,
@@ -16,13 +24,3 @@ def wasserstein_distance_from_kde(p: sm.nonparametric.KDEUnivariate,
     U = interp1d(p.support, p.cdf, kind='nearest', bounds_error=False, fill_value="extrapolate")
     V = interp1d(q.support, q.cdf, kind='nearest', bounds_error=False, fill_value="extrapolate")
     return sum((np.array(domain)[1:] - np.array(domain)[:-1]) * (np.abs(U(domain[1:]) - V(domain[1:]))))
-
-
-def continuous_wasserstein_distance_from_sample(sample_p: np.ndarray,
-                                                sample_q: np.ndarray) -> float:
-    kde_p = sm.nonparametric.KDEUnivariate(sample_p)
-    kde_p.fit()
-    kde_q = sm.nonparametric.KDEUnivariate(sample_q)
-    kde_q.fit()
-
-    return wasserstein_distance_from_kde(kde_p, kde_q)
